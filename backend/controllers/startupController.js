9bfef7ee02480d7464bbcd349203b36deeb09103
chaitanya-kadali <charankadaliaa@gmail.com>
2024-09-17 11:21:13 +0530

    none

--- backend/controllers/startupController.js
+++ backend/controllers/startupController.js
@@ -17,7 +17,7 @@ const upload = multer({ storage: storage });
 
 const jwt = require('jsonwebtoken');  //object to Generate JWT token
 
-
+ 
 
 //Registration for the start up
 exports.Startup_Dashboard = catchAsyncErrors(async (req, res) => {
