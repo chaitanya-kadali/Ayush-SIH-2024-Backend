5d3a0ceb0bb61c9f1bc8337e3f3b29d053e694aa
chaitanya-kadali <charankadaliaa@gmail.com>
2024-09-24 06:18:20 +0530

    modified the capability to hoast 3 local frontend clients

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ require('dotenv').config(); // to access the values .env file
 const app = express();
 // https://ayush-sih-2024-frontend.vercel.app/
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://ayush-sih-2024-frontend.vercel.app/',/^https:\/\/ayush-sai-2024-frontend\.vercel\.app(\/.*)?$/ ],  // Allows requests from this frontend url
+  origin: ['http://localhost:5173','http://localhost:5174','http://localhost:5175', 'https://ayush-sih-2024-frontend.vercel.app/',/^https:\/\/ayush-sai-2024-frontend\.vercel\.app(\/.*)?$/ ],  // Allows requests from this frontend url
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
